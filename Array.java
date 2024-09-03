47c6d0b65025f11bac3d307150551813a3701340
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-03 19:56:01 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
+        //declare and initialize an array
         int[]arr={2,4,7,8,9 };
+        //Acess and print Array element
         System.out.println("acessing elemnt of Arrays");
         System.out.println("first element :"+arr[0]);
         System.out.println("second element :"+arr[1]);
