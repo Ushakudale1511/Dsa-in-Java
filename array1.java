d000ad1116e2e66ab1ad695c7664fdb2260903b5
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-03 19:57:55 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,11 @@
+class array1{
+    public static void main(String[] args) {
+        int marks[]= new  int [3];
+        marks[0]= 78;
+        marks[1]= 98;
+        marks[2]= 88;
+        for(int i = 0; i<=3 ; i++){
+        System.out.println(marks[i]);
+    }
+}
+}
---
+++ array3.java
@@ -0,0 +1,27 @@
+public class array3 {
+    public static void main(String[] args) {
+        // int marks[] = new int [4];
+        // marks[0]= 45;
+        // marks[1]= 65;
+        // marks[2]= 75;
+        // marks[3]= 85;
+        
+        
+        //   for(int i= 0 ; i < marks.length ; i++)
+        //   {
+        //     System.out.println(marks[i]);
+        //   }
+
+            // 2D ARRAy
+            int a [][]= {
+            { 
+                1 , 2
+            } ,{
+                  3 , 4
+            }
+        };
+            System.out.println(a [0 ][0]);
+            }
+        
+
+    }
