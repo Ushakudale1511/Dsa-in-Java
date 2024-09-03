b0f92aa58e387365e09a863e4382428941fb9150
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-03 19:54:12 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Array {
+    public static void main(String[] args) {
+        int[]arr={2,4,7,8,9 };
+        System.out.println("acessing elemnt of Arrays");
+        System.out.println("first element :"+arr[0]);
+        System.out.println("second element :"+arr[1]);
+        System.out.println("Third element :"+arr[2]);
+       
+    }
+    
+}
