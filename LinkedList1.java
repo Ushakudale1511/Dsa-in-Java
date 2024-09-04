fab47888539eda46610fbfc6b434d2fc80545cde
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-09-04 23:51:35 +0530

    Add files via upload

---
+++ LinkedList1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class LinkedList1{
+    public static void main(String[] args) {
+        
+    LinkedList<String>li=new LinkedList<String>();
+    li.add ("usha");
+    li.add ("Nilu");
+    li.add ("Ashu");
+    li.add ("Shobha");
+    iterator<String>itr=li.iterator();
+    while(itr.next());
+    System.out.println(itr.next());
+
+}
+    
+}
